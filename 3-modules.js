88e0318d0284d5595cf3112ece09cba31b40d363
Armin Manlapat <arminmanlapat@gmail.com>
2021-08-12 16:17:02 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const thename = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(thename.john)
+sayHi(thename.peter)
